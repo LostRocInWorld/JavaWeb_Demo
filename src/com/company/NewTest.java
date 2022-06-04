1df8bd2053d0f87f8a4447dc94ee50a95f344181
Dustin_Peng <2713434603@QQ.COM>
2022-06-04 11:45:34 +0530

    新增了一个文件NewTest.java

---
+++ src/com/company/NewTest.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * @Project Javaweb_03jQuery
+ * @Package com.company
+ * @ClassName NewTest
+ * @Description TODO
+ * @Date 2022/6/4   11:43
+ * @Created by Dustin_Peng
+ */
+public class NewTest {
+    public static void main(String[] args) {
+        System.out.println("这是用来测试git的");
+    }
+}
